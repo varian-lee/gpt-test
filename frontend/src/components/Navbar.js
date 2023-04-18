0427057b84da9cf23c1e9bf04eb3c640c80d9bd2
Kihyun Lee <varian.k.lee@gmail.com>
2023-04-18 16:11:16 +0900

    의미없는 줄추가

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -54,7 +54,7 @@ export default function Navbar () {
               <button type={"button"}
                       className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
                       onClick={handleError}
-              >ErrorTab</button>
+              >ErrorTab</button> 
             </li>
             <li>
               <button type={"button"}
