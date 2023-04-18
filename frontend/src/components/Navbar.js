3d282ee3352cac35565a6b69f7b943b0d24c518c
Kihyun Lee <varian.k.lee@gmail.com>
2023-04-18 14:45:28 +0900

    NavBar 에 에러탭 추가

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -34,6 +34,11 @@ export default function Navbar () {
     }
   }
 
+  function handleError() {
+    const tempValue = undefined;
+    const letsMakeError = tempValue.notExsistingVariable[0];
+  }
+
   return (
 
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded">
@@ -43,6 +48,15 @@ export default function Navbar () {
         </Link>
         <Hamburger onClick={handleClickHamburger} />
         <div ref={menuRef} className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul
+            className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
+            <li>
+              <button type={"button"}
+                      className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+                      onClick={handleError}
+              >ErrorTab</button>
+            </li>
+          </ul>
           <ul
             className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
             <li>
